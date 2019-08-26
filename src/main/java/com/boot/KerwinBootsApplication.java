6b47bb5d00d8d9e0415ca7ea955dccd12bd48b4a
柯贤铭 <kexianming@testin.cn>
2019-08-26 17:09:06 +0800

    初此提交 master分支

---
+++ src/main/java/com/boot/KerwinBootsApplication.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerwinBootsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KerwinBootsApplication.class, args);
+    }
+
+}
