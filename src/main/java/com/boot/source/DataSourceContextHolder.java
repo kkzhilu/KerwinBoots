592cf7a2711b230a51f2cf3c9b79bac7782ce09f
kkzhilu <34807944+kkzhilu@users.noreply.github.com>
2019-09-16 23:08:28 +0800

    upd

--- src/main/java/com/boot/source/DataSourceContextHolder.java
+++ src/main/java/com/boot/source/DataSourceContextHolder.java
@@ -14,7 +14,7 @@ final class DataSourceContextHolder {
      * ThreadLocal提供了线程内存储变量的能力，这些变量不同之处在于每一个线程读取的变量是对应的互相独立的
      * 通过get和set方法就可以得到当前线程对应的值
      */
-    private static ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
+    private static final ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
 
     static void setDbType(String dbType) {
         CONTEXT_HOLDER.set(dbType);
