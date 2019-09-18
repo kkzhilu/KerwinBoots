609c0709c4298b92a460a9f2e557dd5367143dce
柯贤铭 <kexianming@testin.cn>
2019-09-18 18:09:27 +0800

    push to jedis demo

---
+++ src/main/java/com/boot/config/JedisConfig.java
@@ -0,0 +1,45 @@
+package com.boot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * ******************************
+ * author：      柯贤铭
+ * createTime:   2019/9/18 17:54
+ * description:  JedisConfig
+ * version:      V1.0
+ * ******************************
+ */
+@Configuration
+public class JedisConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.max-idle}")
+    private Integer maxIdle;
+
+    @Value("${spring.redis.min-idle}")
+    private Integer minIdle;
+
+    @Bean
+    public JedisPool redisPoolFactory(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMinIdle(minIdle);
+        jedisPoolConfig.setMaxWaitMillis(3000L);
+        int timeOut = 3;
+        return  new JedisPool(jedisPoolConfig, host, port, timeOut, password);
+    }
+}
--- src/test/java/com/boot/ApplicationTests.java
+++ src/test/java/com/boot/ApplicationTests.java
@@ -1,86 +1,24 @@
 package com.boot;
 
-import com.boot.util.RedisUtil;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
-import java.security.Key;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KerwinBootsApplication.class)
 public class ApplicationTests {
 
     @Resource
-    RedisTemplate<String,Object> redisTemplate;
-
-    // 数据
-    List<Map<String, Object>> dataList = new ArrayList<>();
-
-    @Before
-    public void initData() {
-        for (int i = 1; i <= 3; i++) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("orderId", i);
-            map.put("money", 30 + (int) (Math.random() * 20));
-            map.put("time", "2018-05-12");
-            map.put("key", "order:" + i);
-            dataList.add(map);
-        }
-    }
+    JedisPool jedisPool;
 
     @Test
-    public void hmset () {
-        for (Map<String, Object> map : dataList) {
-            RedisUtil.hmset(map.get("key").toString(), map);
-        }
-    }
-
-    @Test
-    public void lpush () {
-        for (Map<String, Object> map : dataList) {
-            redisTemplate.opsForList().leftPush("user:1:order", map.get("key"));
-        }
-
-    }
-
-    @Test
-    public void appendHash () {
-        Map<String, Object> map = new HashMap<>();
-        map.put("orderId", 4);
-        map.put("money", 30 + (int) (Math.random() * 20));
-        map.put("time", "2018-01-01");
-        map.put("key", "order:" + 4);
-        RedisUtil.hmset(map.get("key").toString(), map);
-    }
-
-    @Test
-    public void appendSet () {
-        Map<String, Object> map = new HashMap<>();
-        map.put("orderId", 4);
-        map.put("money", 30 + (int) (Math.random() * 20));
-        map.put("time", "2018-01-01");
-        map.put("key", "order:" + 4);
-        dataList.add(map);
-        redisTemplate.opsForList().rightPush("user:1:order", dataList.get(3).get("key").toString());
-    }
-
-    @Test
-    public void findOrder () {
-        List<Object> range = redisTemplate.opsForList().range("user:1:order", 0, -1);
-        System.out.println(range);
-        for (Object o : range) {
-            String key = o.toString();
-            Map<Object, Object> hmget = RedisUtil.hmget(key);
-            System.out.println(hmget);
-        }
+    public void testJedis () {
+        Jedis jedis = jedisPool.getResource();
+        jedis.set("year", String.valueOf(24));
     }
 }
