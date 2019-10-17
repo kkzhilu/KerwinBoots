ca45238c8e9b279fe892be3dd73e5136258680d9
柯贤铭 <kexianming@testin.cn>
2019-10-17 18:58:57 +0800

    重要BUG push

--- src/main/java/com/boot/config/ThreadQueue.java
+++ src/main/java/com/boot/config/ThreadQueue.java
@@ -2,6 +2,7 @@ package com.boot.config;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,9 +29,25 @@ public class ThreadQueue {
      */
     private BlockingQueue<String> queue;
 
+    /**
+     * 初始化
+     */
     public ThreadQueue () {
-        init();
+        // 线程池
+        service = Executors.newFixedThreadPool(10);
 
+        // 队列
+        queue = new LinkedBlockingQueue<>();
+    }
+
+    /*****************************************************************************************
+     * 注意：由于本方法执行的必要参数 queue 是本类构建的, 所以可以正常运行
+     * 如果线程池初始化，立即开始执行任务，使用其他类的属性，则很有可能在 属性赋值之前调用导致空指针
+     * 解决方案: @PostConstruct
+     *            CommandLineRunner接口
+     */
+    @PostConstruct
+    public void postConstruct () {
         // 创造工作线程读取度列
         service.execute(() -> {
             try {
@@ -39,17 +56,6 @@ public class ThreadQueue {
                 e.printStackTrace();
             }
         });
-    }
-
-    /**
-     * 初始化
-     */
-    private void init () {
-        // 线程池
-        service = Executors.newFixedThreadPool(10);
-
-        // 队列
-        queue = new LinkedBlockingQueue<>();
     }
 
     /***
